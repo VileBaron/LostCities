e1a2d461c0d77fc5ff4d00c9d641a638e7ff666b
VileBaron <47114026+VileBaron@users.noreply.github.com>
2019-10-31 12:10:37 -0400

    Initial commit

---
+++ Assets/__Scripts/Card.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Card : MonoBehaviour {
+
+	public string    suit;
+	public int       rank;
+	public Color     color = Color.white;
+	public string    colS = "White";
+	
+	public List<GameObject> decoGOs = new List<GameObject>();
+	public List<GameObject> pipGOs = new List<GameObject>();
+	
+	public GameObject back;  // back of card;
+	public CardDefinition def;  // from DeckXML.xml		
+
+
+	public bool faceUp {
+		get {
+			return (!back.activeSelf);
+		}
+
+		set {
+			back.SetActive(false);
+		}
+	}
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+} // class Card
+
+[System.Serializable]
+public class Decorator{
+	public string	type;			// For card pips, tyhpe = "pip"
+	public Vector3	loc;			// location of sprite on the card
+	public bool		flip = false;	//whether to flip vertically
+	public float 	scale = 1.0f;
+}
+
+[System.Serializable]
+public class CardDefinition{
+	public string	face;	//sprite to use for face cart
+	public int		rank;	// value from 1-10 (Ace-King)
+	public List<Decorator>	
+					pips = new List<Decorator>();  // Pips Used
+}
